633a64701dafd26e553f274f1aead1fd22a22f91
Dmytro Shakh <work.dmytro02@gmail.com>
2023-01-09 15:38:49 +0100

    Update Constants.js

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -1,9 +1,8 @@
 
 //? адресс сервера для авторизации, регистрации, проверки токена
-export const URLServerChecking = 'https://api.chyVacheck.students.nomoredomains.club';
+export const URLServerChecking = 'https://api.chyvacheck.students.nomoredomains.club';
 //? адресс сервера для получение данных
-export const URLServerData = 'https://api.chyVacheck.students.nomoredomains.club';
-export const tokenServerData = '6e341995-26c2-4e13-90fe-459da74d1f67';
+export const URLServerData = 'https://api.chyvacheck.students.nomoredomains.club';
 
 export const consoleMessage = {
   "PUT": 'добавить',
