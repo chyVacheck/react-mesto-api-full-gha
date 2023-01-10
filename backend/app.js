bedb10b36fbce34882cc3dbed9649ba099b71b8f
Dmytro Shakh <work.dmytro02@gmail.com>
2023-01-10 20:59:24 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const { Logger } = require('./middlewares/Logger');
 const { DEFAULT_VALUES } = require('./utils/constants');
 
 // ? объявление порт`а
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
