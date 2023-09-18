5244c5bc0493e8622e240583083ba1f0b0c87f17
Dmytro Shakh <work.dmytro02@gmail.com>
2023-09-18 19:31:17 +0200

    feat + fix: поменять лого, отформатировать footer + поправить стили

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,8 +1,10 @@
 function Footer() {
   const data = new Date().getFullYear();
   return (
-    <footer className="footer" lang="en"><p>© {data} Mesto Russia</p></footer>
-  )
+    <footer className="footer" lang="en">
+      <p>© {data} Mesto</p>
+    </footer>
+  );
 }
 
 export default Footer;
