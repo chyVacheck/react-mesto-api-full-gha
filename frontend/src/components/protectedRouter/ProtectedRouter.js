c8765261cf5b08dfe8a87704289862298ef25a8f
chyVacheck <dmytro.shah.work@gmail.com>
2024-03-21 21:23:37 +0100

    make only backend

--- frontend/src/components/protectedRouter/ProtectedRouter.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-function ProtectedRoute({ loggedIn, children }) {
-  return loggedIn ? children : <Navigate to="/signin" />;
-}
-export default ProtectedRoute;
